f097bff54e2b31c77635b4dca6622b043daf00a2
vivek <110154461+solletivivek@users.noreply.github.com>
2024-06-18 00:54:32 +0530

    started react and created the required components and pages

---
+++ swiggy/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LandingPage from './vendorDashboard/pages/LandingPage'
+import "./App.css"
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage/>
+
+    </div>
+  )
+}
+
+export default App
---
+++ swiggy/src/vendorDashboard/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const LandingPage = () => {
+  return (
+    <>
+        <section className='landingSection'>
+        <NavBar/>
+        
+        </section>
+    </>
+
+  )
+}
+
+export default LandingPage
